dcf0ba03a9edc876b4dd54ed58d34577958de95d
we1shcoding <150596197+we1shcoding@users.noreply.github.com>
2023-12-18 01:44:26 +0900

    Title: 큰 수에서 빼기, Time: 148ms, Memory: 24MB, Status: Passed - CodeTree

--- 231218/큰 수에서 빼기/subtract-from-large-number.py
+++ 231218/큰 수에서 빼기/subtract-from-large-number.py
@@ -1,5 +1,10 @@
 a, b = map(int, input().split())
-if a<b:
-    print(b-a)
-elif a>b:
-    print(a-b)
+if a>b:
+    large = a
+    small = b
+else:
+    large = b
+    small =a
+
+c = large-small
+print(c)

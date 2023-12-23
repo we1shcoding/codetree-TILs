fa51dea495f874fdae6ecdb7c02ac9ce8ec61544
we1shcoding <150596197+we1shcoding@users.noreply.github.com>
2023-12-24 00:58:37 +0900

    Title: 두 사람, Time: 104ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 231224/두 사람/two-person.py
+++ 231224/두 사람/two-person.py
@@ -1,7 +1,12 @@
-a_age, a_sex = map(int, str, input().split())
-b_age, b_sex = map(int, str, input().split())
+a = input().split()
+a_age = int(a[0])
+a_sex = a[1]
 
-if (a_age >= 19 and a_sex == 'M') or (b_age >=19 and b_sex == 'M'):
-    print('1')
+b = input().split()
+b_age = int(a[0])
+b_sex = b[0]
+
+if (a_age >= 19 and a_sex == 'M') or (b_age >= 19 and b_sex == 'M'):
+    print(1)
 else:
-    print('0')
+    print(0)

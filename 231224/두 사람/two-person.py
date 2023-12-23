02b61cd6c2c3351af23b7a69cc6dac15594854b2
we1shcoding <150596197+we1shcoding@users.noreply.github.com>
2023-12-24 00:19:11 +0900

    Title: 두 사람, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 231224/두 사람/two-person.py
+++ 231224/두 사람/two-person.py
@@ -1,5 +1,5 @@
-a_age, a_sex = map(int, str, inpit().split())
-b_age, b_sex = map(int, str, inpit().split())
+a_age, a_sex = map(int, str, input().split())
+b_age, b_sex = map(int, str, input().split())
 
 if (a_age >= 19 and a_sex == 'M') or (b_age >=19 and b_sex == 'M'):
     print('1')

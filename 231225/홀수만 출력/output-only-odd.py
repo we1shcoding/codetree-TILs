6de8950f0e17c89b069c267f7f5b36017b50757c
we1shcoding <150596197+we1shcoding@users.noreply.github.com>
2023-12-25 01:25:20 +0900

    Title: 홀수만 출력, Time: 217ms, Memory: 24MB, Status: Passed - CodeTree

--- 231225/홀수만 출력/output-only-odd.py
+++ 231225/홀수만 출력/output-only-odd.py
@@ -1,3 +1,5 @@
-a, b = map(int, input().split())
-for i in range(a + (a % 2), b + 1, 2):
-    print(i, end=" ")
+a, b = list(map(int,input().split()))
+
+for i in range(a,b+1):
+    if i % 2 == 1:
+        print(i,end = ' ')

3bbf9db7b2fa948912de454f1e5ae511119d7cb1
we1shcoding <150596197+we1shcoding@users.noreply.github.com>
2023-12-28 20:01:09 +0900

    Title: 짝수만 출력 2, Time: 112ms, Memory: 24MB, Status: Passed - CodeTree

--- 231228/짝수만 출력 2/output-only-even-2.py
+++ 231228/짝수만 출력 2/output-only-even-2.py
@@ -2,6 +2,5 @@ b,a = map(int, input().split())
 even_number = b
 
 while even_number >= a:
-    if b % 2 == 0:
-        print(even_number, end=' ')
-    even_number -= 2
+    print(even_number, end=' ')
+    even_number -=2

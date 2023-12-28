59e23ec868e294ff5cfa050c327821d2b045ea24
we1shcoding <150596197+we1shcoding@users.noreply.github.com>
2023-12-28 20:15:51 +0900

    Title: a/b 출력, Time: 117ms, Memory: 24MB, Status: Passed - CodeTree

--- 231228/a/b 출력/a-divide-b.py
+++ 231228/a/b 출력/a-divide-b.py
@@ -1,6 +1,12 @@
 a, b = map(int, input().split())
-print('0.',end='')
-for _ in range(20) :
-   a %= b
-   a *= 10
-   print(a//b,end='')
+print(f"{a//b}.", end="")
+# a를 b로 나눈 나머지를 시작으로
+# 소수점 자리를 하나씩 계산합니다.
+a %= b
+for _ in range(20):
+    # 나머지에 10 곱한 값을 기준으로
+    # b로 나누었을 떄의 몫을 구해줍니다.
+    a *= 10
+    print(a // b, end="")
+    # a를 b로 나눈 나머지를 게속 갱신해줍니다.
+    a %= b

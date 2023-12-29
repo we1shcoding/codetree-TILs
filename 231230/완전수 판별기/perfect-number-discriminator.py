2124530af8b027fb65bf3f272e86b8ebc27c4324
we1shcoding <150596197+we1shcoding@users.noreply.github.com>
2023-12-30 00:09:38 +0900

    Title: 완전수 판별기, Time: 120ms, Memory: 72MB, Status: Wrong Answer - CodeTree

--- 231230/완전수 판별기/perfect-number-discriminator.py
+++ 231230/완전수 판별기/perfect-number-discriminator.py
@@ -1,17 +1,10 @@
-def is_perfect_number(n):
-    divisor_sum = 0
-
-    for i in range(1, n):
-        if n % i == 0:
-            divisor_sum += i
-
-    return divisor_sum == n
-
-# 숫자 입력 받기
 n = int(input())
+sum_val = 0
 
-# 완전수 여부 판단 및 출력
-if is_perfect_number(n):
+for i in range(1, n):
+    if n % i == 0:
+        sum_val += 1
+if sum_val == n:
     print('P')
 else:
     print('N')

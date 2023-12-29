8c6237b98b9c280f5161788ccd2ff7ee19856ed3
we1shcoding <150596197+we1shcoding@users.noreply.github.com>
2023-12-29 20:05:05 +0900

    Title: 369 게임, Time: 121ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 231229/369 게임/369-game.py
@@ -0,0 +1,9 @@
+n = int(input())
+i = 1
+
+while i <= n:
+    if i % 3 == 0 or '3' in str(i) or '6' in str(i) or '9' in str(i):
+        print('0', end=' ')
+    else:
+        print(i, end=' ')
+    i += 1

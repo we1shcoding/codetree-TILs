2c03a7d55688b4f9b792cbaec372fe735eba9118
we1shcoding <150596197+we1shcoding@users.noreply.github.com>
2023-12-29 20:09:52 +0900

    Title: 369 게임, Time: 157ms, Memory: 73MB, Status: Passed - CodeTree

--- 231229/369 게임/369-game.py
+++ 231229/369 게임/369-game.py
@@ -2,7 +2,7 @@ n = int(input())
 i = 1
 
 while i <= n:
-    if i % 3 == 0 or '3' in str(i) or '6' in str(i) or '9' in str(i):
+    if i%3==0 or '3' in str(i) or '6' in str(i) or '9' in str(i):
         print('0', end=' ')
     else:
         print(i, end=' ')

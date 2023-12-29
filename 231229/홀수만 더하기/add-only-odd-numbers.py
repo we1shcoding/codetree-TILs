28dd05b5ec320e5f4dfbae307213ea7f57735d7f
we1shcoding <150596197+we1shcoding@users.noreply.github.com>
2023-12-29 23:37:38 +0900

    Title: 홀수만 더하기, Time: 114ms, Memory: 24MB, Status: Passed - CodeTree

--- 231229/홀수만 더하기/add-only-odd-numbers.py
+++ 231229/홀수만 더하기/add-only-odd-numbers.py
@@ -1,14 +1,7 @@
-def calculate_odd_multiple_of_3_sum(numbers):
-    result_sum = 0
-    for num in numbers:
-        if num % 2 == 1 and num % 3 == 0:
-            result_sum += num
-    return result_sum
-
-# 입력 받기
 n = int(input())
-numbers = [int(input()) for _ in range(n)]
-
-# 홀수이면서 3의 배수인 수들의 합 구하고 출력
-result = calculate_odd_multiple_of_3_sum(numbers)
-print(f"{result}")
+sum_val = 0
+for _ in range(n):
+    a = int(input())
+    if a % 3 == 0 and a % 2 == 1:
+        sum_val += a
+print(sum_val)

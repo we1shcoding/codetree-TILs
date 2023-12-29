5701fdd7cc784f1236f403004ee70b9fdd2939ff
we1shcoding <150596197+we1shcoding@users.noreply.github.com>
2023-12-29 20:23:27 +0900

    Title: 배수의 개수 2, Time: 107ms, Memory: 24MB, Status: Passed - CodeTree

--- 231229/배수의 개수 2/number-of-multipliers-2.py
+++ 231229/배수의 개수 2/number-of-multipliers-2.py
@@ -1,6 +1,7 @@
 cnt = 0
-for i in range(10):
+for _ in range(10):
     num = int(input())
-    if num % 2 == 0:
-        cnt +=1
+    if num % 2 == 1:
+        cnt += 1  # 홀수인 경우 카운트 증가
+
 print(cnt)

c8fb5b4ccda434516b4990680c5a9dcd4bda6189
we1shcoding <150596197+we1shcoding@users.noreply.github.com>
2023-12-31 20:40:19 +0900

    Title: 1까지 나누기, Time: 116ms, Memory: 24MB, Status: Passed - CodeTree

--- 231231/1까지 나누기/divide-by-1.py
+++ 231231/1까지 나누기/divide-by-1.py
@@ -1,15 +1,9 @@
-def count_divisions_to_one(n):
-    total_divisions = 0
-
-    for i in range(1, n + 1):
-        n //= i
-        total_divisions += 1
-
-        if n <= 1:
-            break
-
-    print(f"{total_divisions}")
-
-# 정수 n 입력 받기
 n = int(input())
-count_divisions_to_one(n)
+cnt = 0
+
+for i in range(1, 101):
+    if n <= 1:
+        print(cnt)
+        break
+    n //= i
+    cnt += 1

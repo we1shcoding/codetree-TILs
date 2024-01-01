51c1bf109a0aae01c6863df13aad5d5e090c44a1
we1shcoding <150596197+we1shcoding@users.noreply.github.com>
2024-01-01 17:06:29 +0900

    Title: 3n + 1 수열, Time: 113ms, Memory: 24MB, Status: Passed - CodeTree

--- 240101/3n + 1 수열/3n-plus-1-sequence.py
+++ 240101/3n + 1 수열/3n-plus-1-sequence.py
@@ -1,20 +1,13 @@
 n = int(input())
+count = 0
 while True:
-        # 사용자로부터 자연수 입력 받기
-       
+    if n == 1:
+        break
+
+    if n % 2 == 1:
+        n = 3*n+1
+    else:
+        n=n//2
+    count+=1
         
-        # 0이 입력되면 프로그램 종료
-        if n == 1:
-            break
-        
-        # 콜라츠 시퀀스 계산
-        count = 0
-        while n != 1:
-            if n % 2 == 0:
-                n = n // 2
-            else:
-                n = 3 * n + 1
-            count += 1
-        
-        # 결과 출력
-        print(count)
+print(count)

9f689d296a276226a2d005d43cafc2664cb3928a
we1shcoding <150596197+we1shcoding@users.noreply.github.com>
2024-01-01 18:47:12 +0900

    Title: 모두 3의 배수, Time: 111ms, Memory: 24MB, Status: Passed - CodeTree

--- 240101/모두 3의 배수/all-multiples-of-3.py
+++ 240101/모두 3의 배수/all-multiples-of-3.py
@@ -1,13 +1,14 @@
-sat = True
-
+# 변수 선언 및 입력
+satisfied = True
+	
 for _ in range(5):
-    n = int(input())
-    if n%3==0:
-        continue
-    else:
-        sat = False
-        break
-if sat == True:
-    print('1')
+    # 모든 수가 3의 배수인지 확인합니다.
+    a = int(input())
+    if a % 3 != 0:
+        satisfied = False
+
+#출력
+if satisfied == True:
+    print("1")
 else:
-    print('0')
+    print("0")

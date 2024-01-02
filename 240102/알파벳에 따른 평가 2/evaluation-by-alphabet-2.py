145d38946a50d473d146155e4b8ff2b5c8fed55e
we1shcoding <150596197+we1shcoding@users.noreply.github.com>
2024-01-02 21:26:15 +0900

    Title: 알파벳에 따른 평가 2, Time: 145ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240102/알파벳에 따른 평가 2/evaluation-by-alphabet-2.py
@@ -0,0 +1,14 @@
+s = input()
+
+if s == 'S':
+    print('Superior')
+elif s == 'F':
+    print('Fantastic')
+elif s == 'G':
+    print('Good')
+elif s == 'U':
+    print('Usually')
+elif s == 'E':
+    print('Effort')
+else:
+    print('Failure')

63d8e5a0dea4a8904849c95cd66db036da63bfa0
we1shcoding <150596197+we1shcoding@users.noreply.github.com>
2024-01-04 17:17:29 +0900

    Title: 별표 출력하기 6, Time: 128ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240104/별표 출력하기 6/print-star-6.py
@@ -0,0 +1,17 @@
+n = int(input())
+
+# 모양에 맞게 윗쪽 별을 출력합니다.
+for i in range(n):
+	for _ in range(i):
+		print(" ", end=" ")
+	for _ in range((2 * n) - (2 * i) - 1):
+		print("*", end=" ")
+	print()
+
+# 모양에 맞게 아랫쪽 별을 출력합니다.
+for i in range(n-2, -1, -1):
+	for _ in range(i):
+		print(" ", end=" ")
+	for _ in range((2 * n) - (2 * i) - 1):
+		print("*", end=" ")
+	print()

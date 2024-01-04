76f1424641aaa6b815985cd75cffa3a775175a42
we1shcoding <150596197+we1shcoding@users.noreply.github.com>
2024-01-04 17:16:25 +0900

    Title: 두 개의 직각삼각형, Time: 223ms, Memory: 24MB, Status: Passed - CodeTree

--- 240104/두 개의 직각삼각형/two-right-triangle.py
+++ 240104/두 개의 직각삼각형/two-right-triangle.py
@@ -1,5 +1,12 @@
+# 변수 선언 및 입력
 n = int(input())
+
+# 모양에 맞게 별을 출력합니다.
 for i in range(n):
-    print("*"*(n-i),end ="")
-    print(" "*(2*i),end ="")
-    print("*"*(n-i))
+    for _ in range(n - i):
+        print("*", end="")
+    for _ in range(2 * i):
+        print(" ", end="")
+    for _ in range(n - i):
+        print("*", end="")
+    print()

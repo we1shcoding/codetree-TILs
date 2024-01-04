4eb9994052516911e01266b8cacf2f527069cd9b
we1shcoding <150596197+we1shcoding@users.noreply.github.com>
2024-01-04 17:32:58 +0900

    Title: 열 단위로 반복하여 출력, Time: 108ms, Memory: 24MB, Status: Passed - CodeTree

--- 240104/열 단위로 반복하여 출력/print-in-column.py
+++ 240104/열 단위로 반복하여 출력/print-in-column.py
@@ -1,6 +1,6 @@
 n = int(input())
 
-for i in range(1, n + 1):
-    for j in range(1, n + 1):
-        print(i, end='')
+for i in range(n):
+    for j in range(n):
+        print(i+1, end='')
     print()

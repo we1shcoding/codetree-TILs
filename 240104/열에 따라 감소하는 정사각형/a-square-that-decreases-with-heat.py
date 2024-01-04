ce2b34697dda1a2fe70ceb04c82fb9395956de3e
we1shcoding <150596197+we1shcoding@users.noreply.github.com>
2024-01-04 17:36:23 +0900

    Title: 열에 따라 감소하는 정사각형, Time: 211ms, Memory: 24MB, Status: Passed - CodeTree

--- 240104/열에 따라 감소하는 정사각형/a-square-that-decreases-with-heat.py
+++ 240104/열에 따라 감소하는 정사각형/a-square-that-decreases-with-heat.py
@@ -1,6 +1,6 @@
 n = int(input())
 
-for i in range(1, n + 1):
-    for j in range(n, 0, -1):
-        print(j, end=' ')
+for i in range(n):
+    for j in range(n):
+        print(n-j, end=' ')
     print()

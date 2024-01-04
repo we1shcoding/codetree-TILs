4029d0b7d7b8b55ea20fb72a141c38514f77f93c
we1shcoding <150596197+we1shcoding@users.noreply.github.com>
2024-01-04 17:23:31 +0900

    Title: 별표 출력하기 11, Time: 135ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240104/별표 출력하기 11/print-star-11.py
@@ -0,0 +1,11 @@
+# 변수 선언 및 입력
+n = int(input())
+	
+# 2*n+1칸의 정사각형에서 i, j에 짝수가 들어가면 *을 출력합니다.
+for i in range(2 * n + 1):
+	for j in range(2 * n + 1):
+		if i % 2 == 0 or j % 2 == 0:
+			print("*", end=" ")
+		else:
+			print(" ", end=" ")
+	print()

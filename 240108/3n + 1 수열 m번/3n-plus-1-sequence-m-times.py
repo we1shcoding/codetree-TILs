3b8ff501d26e03d7d943cc614882671b56df56c3
we1shcoding <150596197+we1shcoding@users.noreply.github.com>
2024-01-08 18:47:37 +0900

    Title: 3n + 1 수열 m번, Time: 115ms, Memory: 28MB, Status: Passed - CodeTree

--- 240108/3n + 1 수열 m번/3n-plus-1-sequence-m-times.py
+++ 240108/3n + 1 수열 m번/3n-plus-1-sequence-m-times.py
@@ -1,26 +1,19 @@
+# 변수 선언 및 입력
 m = int(input())
-
-
+	
+# m회 반복합니다.
 for _ in range(m):
-    n = int(input())
-    
-    cnt = 0
-
-    # while n != 1:
-    #     if n%2==0:
-    #         n //= 2
-    #     else:
-    #         n = n * 3 + 1
-    #     cnt +=1
-
-    # print(cnt)
-    
-    for _ in range(n, 10000):
-        if n==1:
-            print(cnt)
-            break
-        if n%2==0:
-            n //= 2
-        else:
-            n = 3 * n + 1
-        cnt += 1
+	# 변수 선언 및 입력
+	n = int(input())
+	ans = 0
+		
+	# n이 1이 될 때까지 3n + 1을 반복합니다.
+	while n != 1:
+		if n % 2 == 0:
+			n /= 2
+		else:
+			n = n * 3 + 1
+			
+		ans += 1
+	
+	print(ans)

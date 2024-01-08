15ecf63c6d2f994011204b6db4c545b25ae4ae87
we1shcoding <150596197+we1shcoding@users.noreply.github.com>
2024-01-08 18:43:53 +0900

    Title: 알파벳 출력하기 2, Time: 247ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240108/알파벳 출력하기 2/print-alphabet-2.py
@@ -0,0 +1,12 @@
+n = int(input())
+cnt = 'A'
+
+for i in range(n):
+    for j in range(i):
+        print(' ', end=' ')
+    for j in range(n - i):
+        print(cnt, end=' ')
+        cnt = chr(ord(cnt) + 1)
+        if ord(cnt) > ord('Z'):
+            cnt = 'A'
+    print()

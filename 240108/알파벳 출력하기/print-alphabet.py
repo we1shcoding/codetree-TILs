a4c9e2d1e5681656fc636c0eaf5a5595a6beedea
we1shcoding <150596197+we1shcoding@users.noreply.github.com>
2024-01-08 18:35:12 +0900

    Title: 알파벳 출력하기, Time: 141ms, Memory: 24MB, Status: Passed - CodeTree

--- 240108/알파벳 출력하기/print-alphabet.py
+++ 240108/알파벳 출력하기/print-alphabet.py
@@ -1,16 +1,10 @@
-def print_alphabet_triangle(n):
-    start_char = ord('A')
-    current_char = start_char
-    
-    for i in range(1, n + 1):
-        row = ""
-        for j in range(i):
-            row += chr(current_char)
-            current_char = (current_char - start_char + 1) % 26 + start_char
-        print(row)
-
-# 입력 받기
 n = int(input())
+cnt = 'A'
 
-# 알파벳 삼각형 출력
-print_alphabet_triangle(n)
+for i in range(n):
+    for j in range(i+1):
+        print(cnt, end='')
+        cnt = chr(ord(cnt)+1)
+        if ord(cnt) > ord('Z'):
+            cnt = 'A'
+    print()

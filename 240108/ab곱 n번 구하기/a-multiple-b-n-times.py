f4720903f36ff4675287c9a86466bd2afdcf91d5
we1shcoding <150596197+we1shcoding@users.noreply.github.com>
2024-01-08 18:46:43 +0900

    Title: ab곱 n번 구하기, Time: 115ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240108/ab곱 n번 구하기/a-multiple-b-n-times.py
@@ -0,0 +1,17 @@
+# 변수 선언 및 입력
+n = int(input())
+
+# n회 반복합니다.
+for _ in range(n):
+	# 변수 선언 및 입력
+	inp = input()
+	arr = inp.split()
+	a = int(arr[0])
+	b = int(arr[1])
+	ans = 1
+		
+	# a부터 b까지 전부 곱한 값을 출력합니다.
+	for i in range(a, b + 1):
+		ans *= i
+		
+	print(ans)

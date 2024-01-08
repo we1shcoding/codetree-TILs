573de395f2c272016f5d9d228bebdc331132b032
we1shcoding <150596197+we1shcoding@users.noreply.github.com>
2024-01-08 18:49:36 +0900

    Title: 소수 구하기, Time: 133ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240108/소수 구하기/get-prime.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        int n =sc.nextInt();
+        int cnt=0;
+       
+       for(int i=1;i<=n;i++){
+            cnt=0;
+            for(int j=1;j<=n;j++){
+                if(i%j==0){
+                    cnt++;
+                }
+            }
+
+            if(cnt==2){
+                System.out.print(i+" ");
+            }
+
+       }
+
+
+    }
+}

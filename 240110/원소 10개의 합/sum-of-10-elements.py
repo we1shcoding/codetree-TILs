6261cfeee6bbf9b82f9c842a87e01f6cb21f11d6
we1shcoding <150596197+we1shcoding@users.noreply.github.com>
2024-01-10 22:52:40 +0900

    Title: 원소 10개의 합, Time: 114ms, Memory: 24MB, Status: Passed - CodeTree

--- 240110/원소 10개의 합/sum-of-10-elements.py
+++ 240110/원소 10개의 합/sum-of-10-elements.py
@@ -1,6 +1,10 @@
-arr = input().split()
-
+# 배열을 구현하여 주어진 수를 입력받습니다.
+arr = list(map(int, input().split()))
 sum_val = 0
-for i in range(10):
-    sum_val += int(arr[i])
+
+# 입력받은 수들을  sum에 더합니다.
+for i in arr:
+	sum_val += i
+	
+# sum을 출력합니다.
 print(sum_val)

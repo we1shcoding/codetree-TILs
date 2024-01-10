69adcaa522906efbc884a4432c6f3c50b1b3e5de
we1shcoding <150596197+we1shcoding@users.noreply.github.com>
2024-01-10 23:40:21 +0900

    Title: 10개 입력받기, Time: 129ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240110/10개 입력받기/receive-10-inputs.py
@@ -0,0 +1,12 @@
+arr = list(map(int, input().split()))
+sum_val = 0
+cnt = 0
+
+for i in arr:
+    if i == 0:
+        break
+    sum_val += i
+    cnt += 1
+
+avg = sum_val / cnt
+print(f'{sum_val} {avg:.1f}')

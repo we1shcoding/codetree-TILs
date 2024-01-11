43dfa280c381af202f0f8eb802bb4c9ace3c2e09
we1shcoding <150596197+we1shcoding@users.noreply.github.com>
2024-01-11 20:04:28 +0900

    Title: 배열에서 원소의 특징, Time: 117ms, Memory: 24MB, Status: Passed - CodeTree

--- 240111/배열에서 원소의 특징/characteristics-of-elements-in-an-array.py
+++ 240111/배열에서 원소의 특징/characteristics-of-elements-in-an-array.py
@@ -1,8 +1,6 @@
-elements = list(map(int, input().split()))
+arr = list(map(int, input().split()))
 
-# 3의 배수가 처음으로 등장하는 인덱스 찾기
-index_of_first_multiple_of_3 = elements.index(next(x for x in elements if x % 3 == 0))
-
-# 3의 배수가 처음으로 등장하는 부분의 바로 앞 원소 출력
-result = elements[index_of_first_multiple_of_3 - 1]
-print(result)
+for i in range(10):
+    if arr[i] % 3 == 0:
+        print(arr[i-1])
+        break

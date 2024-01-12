f3fd0ff34a0241b29a1e7a635a02be4e4634d824
we1shcoding <150596197+we1shcoding@users.noreply.github.com>
2024-01-12 21:43:54 +0900

    Title: 순서대로 구구단, Time: 109ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240112/순서대로 구구단/multiplication-table-in-order.py
+++ 240112/순서대로 구구단/multiplication-table-in-order.py
@@ -1,10 +1,9 @@
 a, b = map(int, input().split())
 
 for i in range(1, 10):
-    if a<b:
         for j in range(a, b+1):
             print(f'{j} * {i} = {i * j}', end = '  ')
-    elif a>b:
-        for j in range(b, a+1):
-            print(f'{j} * {i} = {i * j}', end = '  ')
-    print()
+        if a>b:
+            for j in range(b, a+1):
+                print(f'{j} * {i} = {i * j}', end = '  ')
+        print()

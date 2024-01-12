d7c07d7ad9ad81495b6a866f8a57b9e71caeb210
we1shcoding <150596197+we1shcoding@users.noreply.github.com>
2024-01-12 19:36:57 +0900

    Title: 주사위 놀이, Time: 107ms, Memory: 24MB, Status: Passed - CodeTree

--- 240112/주사위 놀이/play-with-dice.py
+++ 240112/주사위 놀이/play-with-dice.py
@@ -1,13 +1,13 @@
 # 주사위 결과를 10번 입력 받기
-dice_results = list(map(int, input().split()))
+arr = list(map(int, input().split()))
 
 # 1부터 6까지 각 숫자가 몇 번씩 나왔는지 저장할 리스트 초기화
-count_list = [0] * 6
+count_arr = [0] * 7
 
 # 각 숫자의 개수 세기
-for result in dice_results:
-    count_list[result - 1] += 1
+for elem in arr:
+    count_arr[elem] += 1
 
 # 결과 출력
-for i in range(6):
-    print(f"{i + 1} - {count_list[i]}")
+for i in range(1, 7):
+    print(f"{i} - {count_arr[i]}")

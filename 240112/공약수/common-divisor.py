123cd9f892ed467c69b8d840e710b1e4f72a64e6
we1shcoding <150596197+we1shcoding@users.noreply.github.com>
2024-01-12 21:23:45 +0900

    Title: 공약수, Time: 104ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240112/공약수/common-divisor.py
+++ 240112/공약수/common-divisor.py
@@ -1,9 +1,19 @@
-n = int(input())
-a, b = map(int, input().split())
-numbers = [a, b]
-min_number = min(numbers)
+def find_common_divisors(n, numbers):
+    # 주어진 수 중에서 최솟값을 찾아 최대 공약수를 구함
+    min_number = min(numbers)
+    
+    # 최대 공약수의 약수들을 찾아서 반환
+    common_divisors = [i for i in range(1, min_number + 1) if min_number % i == 0]
+    
+    return common_divisors
 
-# 최대 공약수의 약수들을 찾아서 출력
-for i in range(1, min_number + 1):
-    if min_number % i == 0:
-        print(i)
+# 입력 받기
+n = int(input())
+numbers = list(map(int, input().split()))
+
+# 공약수 찾기
+common_divisors = find_common_divisors(n, numbers)
+
+# 결과 출력
+for divisor in common_divisors:
+    print(divisor)

48d7f68a6afa061506049b3305a43f10a8b02307
we1shcoding <150596197+we1shcoding@users.noreply.github.com>
2024-01-13 15:59:28 +0900

    Title: 특정 위치의 문자, Time: 136ms, Memory: 24MB, Status: Passed - CodeTree

--- 240113/특정 위치의 문자/char-in-specific-location.py
+++ 240113/특정 위치의 문자/char-in-specific-location.py
@@ -1,12 +1,11 @@
-from sys import stdin
-arr = ['L', 'E', 'B', 'R', 'O', 'S']
-word = stdin.readline().strip()
+my_array = ['L', 'E', 'B', 'R', 'O', 'S']
 
-key = True
-for i, value in enumerate(arr):
-    if value == word:
-        print(i)
-        key=False
-        break
-if key:
+# 사용자로부터 문자 입력 받기
+user_input = input()
+
+# 입력된 문자가 배열에 있는지 확인하고 인덱스 출력
+if user_input in my_array:
+    position = my_array.index(user_input)
+    print(position)
+else:
     print("None")

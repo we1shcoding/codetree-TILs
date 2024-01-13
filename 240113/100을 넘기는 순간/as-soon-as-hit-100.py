75f9f34b99796e4acda9f1f5362097e923195345
we1shcoding <150596197+we1shcoding@users.noreply.github.com>
2024-01-13 23:35:48 +0900

    Title: 100을 넘기는 순간, Time: 108ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240113/100을 넘기는 순간/as-soon-as-hit-100.py
+++ 240113/100을 넘기는 순간/as-soon-as-hit-100.py
@@ -3,6 +3,7 @@ n = int(input())
 total_sum = 0
 count = 0
 
+# 공백으로 구분된 여러 정수를 입력받아 리스트로 변환
 numbers = list(map(int, input().split()))
 
 for num in numbers:
@@ -12,7 +13,7 @@ for num in numbers:
     if num >= 100:
         break
 
-average = total_sum / count
+average = round(total_sum / count, 1) if count > 0 else 0
 
 print(total_sum)
-print(f'{average:.1f}')
+print(average)

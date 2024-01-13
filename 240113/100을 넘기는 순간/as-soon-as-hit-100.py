9c6d39c8e7a4fe4780c5bca21cb825bb9cd88dd5
we1shcoding <150596197+we1shcoding@users.noreply.github.com>
2024-01-13 23:40:23 +0900

    Title: 100을 넘기는 순간, Time: 148ms, Memory: 24MB, Status: Passed - CodeTree

--- 240113/100을 넘기는 순간/as-soon-as-hit-100.py
+++ 240113/100을 넘기는 순간/as-soon-as-hit-100.py
@@ -4,16 +4,11 @@ n = int(input())
 # 누적 합 변수 초기화
 total_sum = 0
 
-# n개의 정수를 공백으로 구분하여 입력 받아서 누적 합 계산
 numbers = input().split()
 for i in range(n):
     num = int(numbers[i])
     total_sum += num
 
-# 합 출력
+avg = total_sum / n
 print(total_sum)
-
-# 평균 계산 및 출력
-average = total_sum / n
-rounded_average = round(average, 1)
-print(rounded_average)
+print(f'{avg:.1f}')

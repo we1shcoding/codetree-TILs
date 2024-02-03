82e6ecb93dd7a6ed59ef64cd6758215f1b86c71d
we1shcoding <150596197+we1shcoding@users.noreply.github.com>
2024-02-03 21:47:25 +0900

    Title: 문자열의 특정 위치 찾기 2, Time: 145ms, Memory: 24MB, Status: Passed - CodeTree

--- 240203/문자열의 특정 위치 찾기 2/find-specific-location-in-spring-2.py
+++ 240203/문자열의 특정 위치 찾기 2/find-specific-location-in-spring-2.py
@@ -1,15 +1,10 @@
-# 문자열 초기화
-strings = ["apple", "banana", "grape", "blueberry", "orange"]
+# 문자열 리스트를 정의합니다.
+string = ["apple", "banana", "grape", "blueberry", "orange"]
+a = input()
+cnt = 0
 
-# 영문자 입력 받기
-given_char = input()
-
-# 조건을 만족하는 문자열 찾기
-matching_strings = [string for string in strings if given_char in string[2:4]]
-
-# 찾아낸 문자열 출력
-for string in matching_strings:
-    print(string)
-
-# 조건을 만족하는 문자열의 개수 출력
-print(len(matching_strings))
+for i in range(5):
+    if string[i][2] == a or string[i][3] ==a:
+        print(string[i])
+        cnt += 1
+print(cnt)

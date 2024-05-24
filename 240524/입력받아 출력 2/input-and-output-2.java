2a7c8b83983867189f29edb03d4c49924e75fa6c
we1shcoding <150596197+we1shcoding@users.noreply.github.com>
2024-05-24 16:34:18 +0900

    Title: 입력받아 출력 2, Time: 125ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240524/입력받아 출력 2/input-and-output-2.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        sc.useDelimiter("-");
+        int num1 = sc.nextInt();
+        int num2 = sc.nextInt();
+        System.out.print(num1);
+        System.out.print(num2);
+    }
+}

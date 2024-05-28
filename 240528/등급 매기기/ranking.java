5f456bf09feed9622b4a895706da45d35fe0d83c
we1shcoding <150596197+we1shcoding@users.noreply.github.com>
2024-05-28 19:57:45 +0900

    Title: 등급 매기기, Time: 152ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240528/등급 매기기/ranking.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        int num;
+        num = sc.nextInt();
+
+        if(num >= 90) {
+            System.out.println("A");
+        }
+        else if(num >=80) {
+            System.out.println("B");
+        }
+        else if(num>=70) {
+            System.out.println("C");
+        }
+        else if(num>=60) {
+            System.out.println("D");
+        }
+        else {
+            System.out.println("F");
+        }
+    }
+}

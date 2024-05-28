612270ab4162b4e3bc68fe9099815d55205b7161
we1shcoding <150596197+we1shcoding@users.noreply.github.com>
2024-05-28 19:40:17 +0900

    Title: 삼항연산자, Time: 127ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240528/삼항연산자/ternary-operator.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        int grade;
+        grade = sc.nextInt();
+        System.out.println(grade == 100? "pass" : "failure");
+    }
+}

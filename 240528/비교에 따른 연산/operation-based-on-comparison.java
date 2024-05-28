8ab31d6f108de7f6c5ec707f56760f0adad120af
we1shcoding <150596197+we1shcoding@users.noreply.github.com>
2024-05-28 19:29:07 +0900

    Title: 비교에 따른 연산, Time: 120ms, Memory: 10MB, Status: Passed - CodeTree

--- 240528/비교에 따른 연산/operation-based-on-comparison.java
+++ 240528/비교에 따른 연산/operation-based-on-comparison.java
@@ -11,7 +11,7 @@ public class Main {
             System.out.println(a*b);
         }
         else {
-            System.out.println(b%a);
+            System.out.println(b/a);
         }
     }
 }

fecac42295412e8320ab282d25ec0dc6514e7cfd
we1shcoding <150596197+we1shcoding@users.noreply.github.com>
2024-05-29 12:25:22 +0900

    Title: 윤년인가, Time: 122ms, Memory: 10MB, Status: Passed - CodeTree

--- 240529/윤년인가/is-leap-year.java
+++ 240529/윤년인가/is-leap-year.java
@@ -5,11 +5,10 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         int y;
         y = sc.nextInt();
-        if(y%4==0) {
+        
+        if((y % 4 == 0 && y % 100 != 0) || y % 400 == 0)
             System.out.println("true");
-        }
-        else if(y%100 ==0 && y%400 != 0) {
+        else
             System.out.println("false");
-        }
     }
 }

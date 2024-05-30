e6ba19f17b0e7b2eb5a280d1b06dc8a67c6d3688
we1shcoding <150596197+we1shcoding@users.noreply.github.com>
2024-05-30 20:02:05 +0900

    Title: 369 게임, Time: 128ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 240530/369 게임/369-game.java
+++ 240530/369 게임/369-game.java
@@ -9,6 +9,9 @@ public class Main {
             if(i%3==0) {
                 System.out.print(0+ " ");
             }
+            else if(i % 10 == 3 || i % 10 == 6 || i % 10 == 9) {
+                System.out.print(0 + " ");
+            }
             else {
                 System.out.print(i+ " ");
             }

a07f6d8ef0c38d3fde87b01cd193f9eb37961dab
we1shcoding <150596197+we1shcoding@users.noreply.github.com>
2024-06-07 10:43:17 +0900

    Title: 완전수 판별기, Time: 124ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240607/완전수 판별기/perfect-number-discriminator.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        int n;
+        int sum = 0;
+        n = sc.nextInt();
+
+        for(int i = 1; i < n; i++) {
+            if(n%i==0)
+                sum+=1;
+        }
+        if(sum==n)
+            System.out.print("P");
+        else
+            System.out.print("N");
+    }
+}

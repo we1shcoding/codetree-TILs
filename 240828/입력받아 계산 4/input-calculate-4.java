6ce3a93817feff2e82be6713c6f23ab7b3e30dea
Seo Hyeon Kim <150596197+we1shcoding@users.noreply.github.com>
2024-08-28 19:00:25 +0900

    Title: 입력받아 계산 4, Time: 136ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240828/입력받아 계산 4/input-calculate-4.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] arg) {
+        Scanner sc = new Scanner(System.in);
+        int a;
+        a = sc.nextInt();
+        System.out.println(a*2);
+    }
+}

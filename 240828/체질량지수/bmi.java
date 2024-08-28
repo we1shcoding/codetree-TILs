fc33206c0e4f7869401edaf189520d5b856fd738
Seo Hyeon Kim <150596197+we1shcoding@users.noreply.github.com>
2024-08-28 21:01:45 +0900

    Title: 체질량지수, Time: 118ms, Memory: 10MB, Status: Passed - CodeTree

--- 240828/체질량지수/bmi.java
+++ 240828/체질량지수/bmi.java
@@ -4,7 +4,7 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         int h = sc.nextInt();
         int w = sc.nextInt();
-        int bmi = w * 100 * 100 / (h * h);
+        int bmi = w * 10000 / (h * h);
 
         System.out.println(bmi);
         if(bmi >= 25) {

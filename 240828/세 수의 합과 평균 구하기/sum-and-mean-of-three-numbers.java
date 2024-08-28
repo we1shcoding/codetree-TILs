bf0ebf1eebabe9be0678957a86c24b48b7421239
Seo Hyeon Kim <150596197+we1shcoding@users.noreply.github.com>
2024-08-28 20:27:50 +0900

    Title: 세 수의 합과 평균 구하기, Time: 141ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240828/세 수의 합과 평균 구하기/sum-and-mean-of-three-numbers.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] arg) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+
+        System.out.println(a+b+c);
+        System.out.println((a+b+c)/3);
+    }
+}

89723dc1455e267399a92756cd74a85bb0c76a0f
Seo Hyeon Kim <150596197+we1shcoding@users.noreply.github.com>
2024-08-28 19:09:49 +0900

    Title: 실수 입력받아 계산 2, Time: 225ms, Memory: 11MB, Status: Passed - CodeTree

---
+++ 240828/실수 입력받아 계산 2/calculation-by-inputting-a-real-number-2.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] arg) {
+        Scanner sc = new Scanner(System.in);
+        double a;
+        a = sc.nextDouble();
+        System.out.printf("%.2f",a+1.5);
+    }
+}

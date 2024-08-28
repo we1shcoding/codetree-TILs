5fc6bb91c852526c2ae5a4817302c233943df6ff
Seo Hyeon Kim <150596197+we1shcoding@users.noreply.github.com>
2024-08-28 19:28:00 +0900

    Title: 입력받은 값과 합 출력, Time: 117ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240828/입력받은 값과 합 출력/output-of-value-and-sum.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] arg) {
+        Scanner sc = new Scanner(System.in);
+        int a;
+        int b;
+        a = sc.nextInt();
+        b = sc.nextInt();
+        System.out.println(a+" "+b+" "+(a+b));
+    }
+}

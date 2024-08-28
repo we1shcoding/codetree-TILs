27236163e4c282a97d7c124f9d8aee80b6b1d0ce
Seo Hyeon Kim <150596197+we1shcoding@users.noreply.github.com>
2024-08-28 18:57:57 +0900

    Title: 점수 입출력, Time: 119ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240828/점수 입출력/enter-int-and-print-score.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        int n;
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        System.out.println("Your score is "+n+" point.");
+    }
+}

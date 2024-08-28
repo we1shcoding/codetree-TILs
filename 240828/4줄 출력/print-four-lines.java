bb5ab94d77f9a0c4d318d53bba3b723e98a994f9
Seo Hyeon Kim <150596197+we1shcoding@users.noreply.github.com>
2024-08-28 15:57:18 +0900

    Title: 4줄 출력, Time: 84ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240828/4줄 출력/print-four-lines.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        System.out.println("Height");
+        System.out.println("200 Cm");
+        System.out.println("Weight");
+        System.out.println("138.723 Kg");
+    }
+}

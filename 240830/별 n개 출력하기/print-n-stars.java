c32d12aec74e05a675caa231a2666c7ce0769c82
Seo Hyeon Kim <150596197+we1shcoding@users.noreply.github.com>
2024-08-30 11:01:00 +0900

    Title: 별 n개 출력하기, Time: 155ms, Memory: 10MB, Status: Passed - CodeTree

--- 240830/별 n개 출력하기/print-n-stars.java
+++ 240830/별 n개 출력하기/print-n-stars.java
@@ -5,7 +5,7 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         int i = 1;
-        while(i<=5) {
+        while(i<=n) {
             System.out.println("*");
             i++;
         }

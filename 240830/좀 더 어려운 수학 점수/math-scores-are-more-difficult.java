6efe3c391c90810f6daa7a4761d42403eab05d01
Seo Hyeon Kim <150596197+we1shcoding@users.noreply.github.com>
2024-08-30 09:50:03 +0900

    Title: 좀 더 어려운 수학 점수, Time: 132ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240830/좀 더 어려운 수학 점수/math-scores-are-more-difficult.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+            int mathA, engA;
+            int mathB, engB;
+            mathA = sc.nextInt();
+            engA = sc.nextInt();
+            mathB = sc.nextInt();
+            engB = sc.nextInt();
+// 영어 점수와 상관없이 수학점수가 높다면 더 높은 학생의 이름 출력
+// 수학점수가 같다면 영어점수가 더 높은 학생 이름 출력
+            if(mathA > mathB || (mathA == mathB && mathA > mathB)) {
+                System.out.println("A");
+            }
+            else {
+                System.out.println("B");
+            }
+        }
+    }

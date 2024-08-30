362c29959291ec69b04be55635f3d7c49bd9ef8f
Seo Hyeon Kim <150596197+we1shcoding@users.noreply.github.com>
2024-08-30 10:21:29 +0900

    Title: 윤년인가, Time: 118ms, Memory: 10MB, Status: Passed - CodeTree

--- 240830/윤년인가/is-leap-year.java
+++ 240830/윤년인가/is-leap-year.java
@@ -1,21 +1,18 @@
 import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        // 여기에 코드를 작성해주세요.
         Scanner sc = new Scanner(System.in);
-        int y = sc.nextInt();
 
-        if(y%4 == 0) {
+        // 변수 선언
+        int y;
+        
+        // 입력
+        y = sc.nextInt();
+        
+        if((y % 4 == 0 && y % 100 != 0) || y % 400 == 0)
             System.out.println("true");
-        }
-        else if(y%400 != 0) {
+        else
             System.out.println("false");
-        }
-        else if(y%100 == 0) {
-            System.out.println("true");
-        }
-        else {
-            System.out.println("false");
-        }
     }
 }

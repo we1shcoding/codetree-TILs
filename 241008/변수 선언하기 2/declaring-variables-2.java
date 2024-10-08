64f7c6f4b49a16ac2c21b0666a49c6318bc42834
Seo Hyeon Kim <150596197+we1shcoding@users.noreply.github.com>
2024-10-08 23:19:08 +0900

    Title: 변수 선언하기 2, Time: 89ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 241008/변수 선언하기 2/declaring-variables-2.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        int x = 365;
+        char y = 'Y';
+        System.out.println(x);
+        System.out.println(y);
+    }
+}

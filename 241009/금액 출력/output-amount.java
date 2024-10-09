30c878196fa1a48ca86b3a95d635c4cac4423348
Seo Hyeon Kim <150596197+we1shcoding@users.noreply.github.com>
2024-10-09 15:34:44 +0900

    Title: 금액 출력, Time: 148ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 241009/금액 출력/output-amount.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] arg) {
+        Scanner sc = new Scanner(System.in);
+        int n;
+        n = sc.nextInt();
+        System.out.println("You have "+n+" won.");
+    }
+}

5b7fc3fa85d1adb17e4ceb7811ab0aa1613f9140
Seo Hyeon Kim <150596197+we1shcoding@users.noreply.github.com>
2024-10-09 16:36:15 +0900

    Title: 정수와 실수의 곱셈, Time: 82ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 241009/정수와 실수의 곱셈/multiplication-of-integers-and-real-numbers.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        int a = 9;
+        double b = 358.273;
+
+        System.out.printf("%d * %.3f = %.3f", a,b,a*b);
+    }
+}

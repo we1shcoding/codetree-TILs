b0ba07dc306e2d97850eca8adcdcee53939d382d
Seo Hyeon Kim <150596197+we1shcoding@users.noreply.github.com>
2024-10-09 18:39:13 +0900

    Title: 남녀노소 구분짓기 2, Time: 124ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 241009/남녀노소 구분짓기 2/sex-and-age-2.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        int gender = sc.nextInt();
+        int age = sc.nextInt();
+        if(gender==0) {
+            if(age>=19) {
+            System.out.println("M");
+            }
+            else {
+                System.out.println("B");
+            }
+        }
+    
+        if(gender==1) {
+            if(age>=19) {
+            System.out.println("W");
+            }
+            else {
+                System.out.println("G");
+            }
+        }
+    }
+}

710db972432d5efa86998bef08ad5078a221f1a5
Seo Hyeon Kim <150596197+we1shcoding@users.noreply.github.com>
2024-10-09 18:57:46 +0900

    Title: 차례로 출력 3, Time: 88ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 241009/차례로 출력 3/print-in-order-3.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        for(int i = 3690; i>=3676; i--)
+        {
+            System.out.print(i+" ");
+        }
+    }
+}

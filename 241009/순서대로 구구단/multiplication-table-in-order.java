7296a52d8504dea6bdadf658eeb833da67667ab1
Seo Hyeon Kim <150596197+we1shcoding@users.noreply.github.com>
2024-10-09 19:56:39 +0900

    Title: 순서대로 구구단, Time: 137ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 241009/순서대로 구구단/multiplication-table-in-order.java
+++ 241009/순서대로 구구단/multiplication-table-in-order.java
@@ -9,20 +9,10 @@ public class Main {
         int b = sc.nextInt();
 
         // a가 b보다 클 경우 swap
-        if (a > b) {
             for (int i = 1; i <= 9; i++) {
                 System.out.printf("%d * %d = %d  ", a, i, a * i); // a의 구구단 출력
                 System.out.printf("%d * %d = %d%n", b, i, b * i); // b의 구구단 출력
-            }
-        }
-        else {
-                   for (int i = 1; i <= 9; i++) {
-                System.out.printf("%d * %d = %d  ", a, i, a * i); // a의 구구단 출력
-                System.out.printf("%d * %d = %d%n", b, i, b * i); // b의 구구단 출력
-            }
-        }
-
-        // 구구단 출력
 
     }
+}
 }

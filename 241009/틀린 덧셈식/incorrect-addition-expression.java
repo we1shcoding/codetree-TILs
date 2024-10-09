8e9cf60f4f1def67669c84648864f09785e1be8a
Seo Hyeon Kim <150596197+we1shcoding@users.noreply.github.com>
2024-10-09 15:48:47 +0900

    Title: 틀린 덧셈식, Time: 109ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 241009/틀린 덧셈식/incorrect-addition-expression.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        // 세 개의 정수형 변수 선언 및 값 대입
+        int a = 123;
+        int b = 43;
+        int c = 80; // 틀린 결과를 변수로 대입
+
+        // 틀린 덧셈식 출력
+        System.out.printf("%d + %d = %d\n", a, b, c);
+    }
+}

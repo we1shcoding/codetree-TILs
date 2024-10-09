a9a9bda881a47dfb9d011250142638a2511ddead
Seo Hyeon Kim <150596197+we1shcoding@users.noreply.github.com>
2024-10-09 15:24:23 +0900

    Title: 달에서 무게 구하기 2, Time: 100ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 241009/달에서 무게 구하기 2/weight-on-the-moon-2.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        int weight = 147;
+        float wf = 0.165f;
+
+        System.out.printf("%d * %.5f = %.5f",weight, wf, weight*wf);
+    }
+}

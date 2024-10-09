00f2013d25c83218b9c3847b78c469b7680fc9e9
Seo Hyeon Kim <150596197+we1shcoding@users.noreply.github.com>
2024-10-09 16:40:44 +0900

    Title: 길이 단위 변환하기 2, Time: 89ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 241009/길이 단위 변환하기 2/change-length-unit-2.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        double yd = 91.44;
+        int mi = 160934;
+
+        System.out.printf("6.8yd = %.1fcm\n", yd*6.8);
+        System.out.printf("0.4mi = %.1fcm", mi*0.4);
+    }
+}

c9ef0ac0d9a5c6f0f4406fe258fc13a8a96fcd0a
Seo Hyeon Kim <150596197+we1shcoding@users.noreply.github.com>
2024-10-18 20:18:40 +0900

    Title: 평균점수 구하기, Time: 158ms, Memory: 11MB, Status: Passed - CodeTree

---
+++ 241018/평균점수 구하기/print-average.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        double[] arr = new double[8];
+
+        double sum = 0;
+
+        for(int i = 0; i < 8; i++)
+        {
+            arr[i] = sc.nextDouble();
+            sum += arr[i];
+        }
+
+        System.out.printf("%.1f", sum/8.0);
+        
+
+    }
+}

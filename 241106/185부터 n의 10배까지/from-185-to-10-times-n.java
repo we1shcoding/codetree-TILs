0c80b546f82911fa4f30e0f62cdb60a5b0e2b043
Seo Hyeon Kim <150596197+we1shcoding@users.noreply.github.com>
2024-11-06 21:45:07 +0900

    Title: 185부터 n의 10배까지, Time: 118ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 241106/185부터 n의 10배까지/from-185-to-10-times-n.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int a = 185;
+        int b = n*10;
+        int sum = 0;
+        int cnt = 0;
+
+        for(int i = a; i<=b; i++) {
+            sum+=i;
+            cnt++;
+        }
+        double avg = (double)sum/cnt;
+        System.out.printf("%d %.1f", sum, avg);
+    }
+}
